b9f3a2eac312f2ba0f916a9f27be673737a4d2df
smpdm-hub <database.smpdm@gmail.com>
2026-07-14 10:42:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://github.com/smpdm-hub/
 export default defineConfig({
   plugins: [react()],
-  base: '/apps/', // Ganti menjadi nama repository Anda
+  // Jika deploy ke Github Pages, base URL harus sesuai dengan nama repository
+  // Hapus baris base ini jika deploy ke Vercel atau root domain
+  base: './', 
+  build: {
+    target: 'esnext'
+  },
+  esbuild: {
+    target: 'esnext'
+  }
 })
